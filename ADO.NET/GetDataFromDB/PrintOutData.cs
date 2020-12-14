d17dd264657708f90a1c813c465787c5255e0625
Vladik750 <zhuk.vlad2000@gmail.com>
2020-12-15 01:58:21 +0200

    Add data printer

---
+++ ADO.NET/GetDataFromDB/PrintOutData.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Linq;
+using ADO.NET.Models;
+
+namespace ADO.NET.GetDataFromDB
+{
+    public static class PrintOutData
+    {
+        public static void print()
+        {
+            var dbContext = new webContext();
+
+            var users = dbContext.User.ToList();
+            var movies = dbContext.Movie.ToList();
+            var movies_rates = dbContext.MovieRate.ToList();
+            var comments = dbContext.Comment.ToList();
+            var comments_rates = dbContext.CommentRate.ToList();
+
+
+            Console.WriteLine("###Users###");
+            foreach (var u in users)
+            {
+                Console.WriteLine(u.UserLogin + " " + u.EncodedPass + " " + u.Name + " " + u.Status);
+
+            }
+            Console.WriteLine("");
+            Console.WriteLine("###Movies###");
+            foreach (var m in movies)
+            {
+                Console.WriteLine(m.Id + " " + m.Name + " " + m.LenInSec);
+
+            }
+            Console.WriteLine("");
+            Console.WriteLine("###MoviesRates###");
+            foreach (var mr in movies_rates)
+            {
+                Console.WriteLine(mr.Id + " " + mr.UserLogin + " " + mr.MovieId + " " + mr.Rate + " " + mr.Comment);
+
+            }
+            Console.WriteLine("");
+            Console.WriteLine("###Comments###");
+            foreach (var c in comments)
+            {
+                Console.WriteLine(c.Id + " " + c.UserLogin + " " + c.MovieId + " " + c.Text + " " + c.Timestamp);
+
+            }
+            Console.WriteLine("");
+            Console.WriteLine("###Comments_rates###");
+            foreach (var cr in comments_rates)
+            {
+                Console.WriteLine(cr.Id + " " + cr.UserLogin + " " + cr.CommentId + " " + cr.Rate);
+
+            }
+        }
+    }
+}
