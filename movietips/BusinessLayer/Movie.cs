82b7de6c4d69cb656a23b9a6d56c7a299405d9ff
Vladik750 <zhuk.vlad2000@gmail.com>
2020-12-16 22:10:56 +0200

    Upgraded Movie class

--- movietips/BusinessLayer/Movie.cs
+++ movietips/BusinessLayer/Movie.cs
@@ -9,22 +9,23 @@ namespace BusinessLayer
 {
     public class Movie
     {
-        public int id;
+        int id;
+        CommentsList comments; 
 
         public Movie(string moviename_input, int leninsec )
         {
             if(dbContext.Movie.Any(m => m.Name == moviename_input))
             {
                 id = dbContext.Movie.FirstOrDefault(m => m.Name == moviename_input).Id;
-                
             }else{
                 var max_id=dbContext.Movie.Max(m => m.Id);
                 
                 DataLayer.Models.Movie new_movie = new DataLayer.Models.Movie{Id = max_id+1, Name = moviename_input, LenInSec = leninsec  };
                 dbContext.Movie.Add(new_movie);
-                dbContext.SaveChanges();
-                
+                dbContext.SaveChanges(); 
+                id = max_id+1;
             }
+            comments = new CommentsList(id);
         }
     }
 }
