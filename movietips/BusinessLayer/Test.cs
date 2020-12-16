de8f6bcccd1623e391c823ccbec0c3451d008f75
Vladik750 <zhuk.vlad2000@gmail.com>
2020-12-17 00:56:59 +0200

    SimpleMovie debugging

--- movietips/BusinessLayer/Test.cs
+++ movietips/BusinessLayer/Test.cs
@@ -6,8 +6,7 @@ namespace BusinessLayer
     {
         public static void Main(string[] args)
         {
-            Movie nm = new Movie("movie_10");
-            Console.WriteLine(nm.comments[0].getCommentText());
+            
         }
     }
 }
