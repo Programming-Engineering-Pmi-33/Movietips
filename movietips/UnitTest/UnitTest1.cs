5ff94f547e9e186bad5cc97580d346dfe1ccf379
Vladik750 <zhuk.vlad2000@gmail.com>
2020-12-17 13:31:42 +0200

    Upgraded Login class and UnitTest

--- movietips/UnitTest/UnitTest1.cs
+++ movietips/UnitTest/UnitTest1.cs
@@ -1,17 +1,63 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using BusinessLayer;
-using DataLayer;
-
+using static DataLayer.DBContext;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 namespace UnitTest
 {
     [TestClass]
-    public class UnitTest
+    public class UnitTest1
     {
         [TestMethod]
         public void TestLogin()
         {
-            Login result = new User("yura","pass");//test for existing username
-            Assert.isTrue(result, String.Format("Expected for '{1}':false; Actual: {0}"));
+            Login t = new Login();
+            Assert.IsTrue(t.user_check("yura", "pass"));
+        }
+        [TestMethod]
+        public void TestLogin1()
+        {
+            Login t1 = new Login();
+            Assert.IsFalse(t1.user_check("maxim","pass"));
+        }
+
+        [TestMethod]
+        public void TestMovie()
+        {
+            BusinessLayer.Movie m = new BusinessLayer.Movie("ForestGump");
+            var mov = dbContext.Movie.First(m=> m.Name == "ForestGump");
+            Assert.IsTrue(mov.Name== "ForestGump");
+        }
+
+        [TestMethod]
+        public void TestMovie2()
+        {
+            BusinessLayer.Movie m = new BusinessLayer.Movie("ForestGump");
+            var mov = dbContext.Movie.First(m=> m.Name == "ForestGump");
+            Assert.IsTrue(mov.Id== 53);
+        }
+
+        [TestMethod]
+        public void TestMovieTip()
+        {
+            BusinessLayer.MovieTip mt = new BusinessLayer.MovieTip("some tip","yura", 53, 77);
+            mt.commitTipToDB();
+            Assert.IsTrue(mt.getCommentText() == "some tip");
+        }
+        
+        [TestMethod]
+        public void TestRegistration()
+        {
+            Login t = Registration.registrate("User_arsen7","pass","Arsen");
+            Assert.IsTrue(t.getStatus() == "ok");
+        }
+
+        [TestMethod]
+        public void TestRegistration1()
+        {
+            Login t = Registration.registrate("User_arsen7","pass","Arsen");
+            Assert.IsTrue(t.getStatus() == "User already exists");
         }
     }
 }
